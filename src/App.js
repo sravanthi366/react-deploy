ac2aef80599297ddf6f03f47d3d3fc353cc701de
sravanthi366 <sravanthimca95@gmail.com>
2020-12-24 15:51:45 +0530

    deploy react in git pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import ProfileImage from './img/WhatsApp Image 2020-12-24 at 11.38.30 AM.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App</h1>
+      <h2>Author: sravanthi boddu</h2>
+      <img src={ProfileImage} alt="profile-image"></img>
     </div>
   );
 }
